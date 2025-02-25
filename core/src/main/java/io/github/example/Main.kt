1b1a4b160e0f093db6d1455d45d0b82a01464424
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-25 19:10:19 +0300

    init

---
+++ core/src/main/java/io/github/example/Main.kt
@@ -0,0 +1,20 @@
+package io.github.example
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+class Main : ApplicationAdapter() {
+
+
+    override fun create() {
+        super.create()
+    }
+
+    override fun render() {
+        super.render()
+    }
+
+    override fun dispose() {
+        super.dispose()
+    }
+}
