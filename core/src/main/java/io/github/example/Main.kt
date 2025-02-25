eef9bba00ab9c5ab2ce2908ac0edcafd2c1f53d4
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-02-25 19:13:32 +0300

    add chessTable

--- core/src/main/java/io/github/example/Main.kt
+++ core/src/main/java/io/github/example/Main.kt
@@ -1,9 +1,13 @@
 package io.github.example
 
 import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.utils.Array
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 class Main : ApplicationAdapter() {
+
+
+    val chessTable = Array<GameObject>(64)
 
 
     override fun create() {
