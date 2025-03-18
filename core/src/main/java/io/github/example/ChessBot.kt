272ffc25d3773ab642e3ecc37013aaab03f213f5
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-03-18 18:55:09 +0300

    add ChessBot

---
+++ core/src/main/java/io/github/example/ChessBot.kt
@@ -0,0 +1,9 @@
+package io.github.example
+
+import com.badlogic.gdx.graphics.Color
+
+class ChessBot(val color: Color) {
+    fun getBestMove(board: Array<Array<ChessPiece?>>): Pair<Int, Int>? {
+        return null
+    }
+}
