c22dee142e0956b73ae4be20c8674f1bb46d9bf3
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-03-20 20:22:32 +0300

    add getMovePieces

--- core/src/main/java/io/github/example/ChessBot.kt
+++ core/src/main/java/io/github/example/ChessBot.kt
@@ -3,7 +3,31 @@ package io.github.example
 import com.badlogic.gdx.graphics.Color
 
 class ChessBot(val color: Color) {
+
+    private val opponentColor: Color
+        get() = if(color == Color.WHITE) Color.WHITE else Color.BLACK
+
     fun getBestMove(board: Array<Array<ChessPiece?>>): Pair<Int, Int>? {
         return null
+    }
+
+
+    private fun getMovePieces(board: Array<Array<ChessPiece?>>): List<Move> {
+
+        val moves = mutableListOf<Move>()
+
+        for (row in board.indices) {
+            for (col in board[row].indices) {
+                val piece = board[row][col]
+                if (piece != null && piece.color == color){
+                    val validMoves = piece.getValidMoves(row, col, board)
+
+                    for (move in validMoves) {
+                        moves.add(Move(move.first, move.second))
+                    }
+                }
+            }
+        }
+        return moves
     }
 }
