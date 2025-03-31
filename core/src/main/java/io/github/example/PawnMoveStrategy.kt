e66df534568f1f86e94e0671f370a62ac12861cb
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-03-31 20:02:28 +0300

    delete commentaries in PawnMoveStrategy

--- core/src/main/java/io/github/example/PawnMoveStrategy.kt
+++ core/src/main/java/io/github/example/PawnMoveStrategy.kt
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.Color
 class PawnMoveStrategy : MoveStrategy {
     override fun getValidMoves(row: Int, col: Int, board: Array<Array<ChessPiece?>>): List<Pair<Int, Int>> {
         val moves = mutableListOf<Pair<Int, Int>>()
-        val direction = if (board[row][col]?.color == Color.WHITE) -1 else 1 // направление движения
+        val direction = if (board[row][col]?.color == Color.WHITE) -1 else 1
 
 
         if (board[row + direction][col] == null) {
