95bcab64114ca05b2213a3f8d50bc23fc6ac1a2b
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-03-31 20:00:17 +0300

    optimization PawnMoveStrategy

--- core/src/main/java/io/github/example/PawnMoveStrategy.kt
+++ core/src/main/java/io/github/example/PawnMoveStrategy.kt
@@ -7,12 +7,11 @@ class PawnMoveStrategy : MoveStrategy {
         val moves = mutableListOf<Pair<Int, Int>>()
         val direction = if (board[row][col]?.color == Color.WHITE) -1 else 1 // направление движения
 
-        // Проверка на пустую ячейку впереди
+
         if (board[row + direction][col] == null) {
             moves.add(Pair(row + direction, col))
         }
 
-        // Добавьте другие условия для валидных движений
 
         return moves
     }
