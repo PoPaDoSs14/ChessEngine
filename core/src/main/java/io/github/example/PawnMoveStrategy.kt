17cff4520b102c085985f7192bf6c2828ebc4b84
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-04-01 19:19:43 +0300

    add first move pawn

--- core/src/main/java/io/github/example/PawnMoveStrategy.kt
+++ core/src/main/java/io/github/example/PawnMoveStrategy.kt
@@ -3,6 +3,9 @@ package io.github.example
 import com.badlogic.gdx.graphics.Color
 
 class PawnMoveStrategy : MoveStrategy {
+
+    private var firstMove = 0
+
     override fun getValidMoves(row: Int, col: Int, board: Array<Array<ChessPiece?>>): List<Pair<Int, Int>> {
         val moves = mutableListOf<Pair<Int, Int>>()
         val direction = if (board[row][col]?.color == Color.WHITE) -1 else 1
@@ -11,6 +14,8 @@ class PawnMoveStrategy : MoveStrategy {
         if (board[row + direction][col] == null) {
             moves.add(Pair(row + direction, col))
         }
+
+
 
 
         return moves
