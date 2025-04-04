d516ba57d59d7a7a3fddca794ccbd1cb70d89e08
PoPaDoSs14 <slasevvladimir7@gmail.com>
2025-04-04 20:56:42 +0300

    fix bug

--- core/src/main/java/io/github/example/PawnMoveStrategy.kt
+++ core/src/main/java/io/github/example/PawnMoveStrategy.kt
@@ -4,22 +4,30 @@ import com.badlogic.gdx.graphics.Color
 
 class PawnMoveStrategy : MoveStrategy {
 
-    private var firstMove = 0
+    private var firstMove = true
 
     override fun getValidMoves(row: Int, col: Int, board: Array<Array<ChessPiece?>>): List<Pair<Int, Int>> {
         val moves = mutableListOf<Pair<Int, Int>>()
         val direction = if (board[row][col]?.color == Color.WHITE) -1 else 1
 
-
-        if (board[row + direction][col] == null) {
+        if (isValidPosition(row + direction, col, board) && board[row + direction][col] == null) {
             moves.add(Pair(row + direction, col))
         }
 
-        if (board[row + direction][col] == null && firstMove <= 0) {
-            moves.add(Pair(row + direction + direction, col))
+        if (firstMove && isValidPosition(row + 2 * direction, col, board) && board[row + 2 * direction][col] == null) {
+            if (board[row + direction][col] == null) {
+                moves.add(Pair(row + 2 * direction, col))
+            }
         }
 
-
         return moves
+    }
+
+    fun markFirstMoveDone() {
+        firstMove = false
+    }
+
+    private fun isValidPosition(row: Int, col: Int, board: Array<Array<ChessPiece?>>): Boolean {
+        return row in board.indices && col in board[row].indices
     }
 }
